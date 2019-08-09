4a8317ec7bd79a8113b6e2f6b2bbe2c07998e460
Wes Casto <wescasto@gmail.com>
2019-08-08 22:53:50 -0400

    Added variables to track first and second clicks

--- js/timecard.js
+++ js/timecard.js
@@ -1,26 +1,29 @@
 var blocks = [];
 var htmlOutput = '';
+var firstSelection;
+var secondSelection;
 
 // display today's date
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 var today  = new Date();
 var dateDisplay = document.getElementById('date');
-dateDisplay.innerText = today.toLocaleDateString("en-US", options);
+dateDisplay.innerText = today.toLocaleDateString('en-US', options);
 
 // create grid
-for (let i = 0; i < 4; i++) {
-    htmlOutput += '<div id="' + i + '" class="block empty">B</div>';
+for (let i = 0; i < 6; i++) {
+    htmlOutput += '<div id="' + i + '" class="block empty"></div>';
     blocks.push({
         id: 'block' + i,
-        status: 'empty'
+        status: 'empty',
+        duration: 15
     });
 }
 
 // output blocks to DOM
 var grid = document.getElementById('grid');
-grid.innerHTML = htmlOutput;
+grid.innerHTML = htmlOutput; // TODO: use nodes instead of innerHTML
 
-// register click listeners
+// add click listeners
 document.querySelectorAll('.block').forEach(item => {
     item.addEventListener('click', event => {
         handleBlockClick(item);
@@ -28,14 +31,14 @@ document.querySelectorAll('.block').forEach(item => {
 });
 
 function handleBlockClick(item) {
-    blocks[item.id].status = 'clicked';
-    item.classList.add('clicked');
-    console.log(blocks);
-}
+    blocks[item.id].status = 'active';
+    item.classList.add('active');
 
-// output all blocks
-/*
-blocks.forEach(function(item, index, array) {
-    console.log(item, index);
-});
-*/
+    if (firstSelection == null) {
+        firstSelection = blocks[item.id];
+        console.log(firstSelection);
+    } else {
+        secondSelection = blocks[item.id];
+        console.log(secondSelection);
+    }
+}
